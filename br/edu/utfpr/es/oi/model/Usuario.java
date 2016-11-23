4d7904fedffaf5dd9c5dc94623aae9a3c74d6475
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2016-11-23 06:13:50 -0200

    adicionado senha e id para Usuario, visto que precisaremos o id para edição dos dados.

--- br/edu/utfpr/es/oi/model/Usuario.java
+++ br/edu/utfpr/es/oi/model/Usuario.java
@@ -7,14 +7,72 @@ import java.util.List;
 
 public class Usuario {
 
-	private String nome;
-	private String email;
-	private String areainteresse;
-	private String departamento;
-	private List<TempoLivre> tempoLivre;
+    private int id;
+    private String nome;
 
-	public void crudTempoLivre() {
+    public int getId() {
+        return id;
+    }
 
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getAreainteresse() {
+        return areainteresse;
+    }
+
+    public void setAreainteresse(String areainteresse) {
+        this.areainteresse = areainteresse;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public List<TempoLivre> getTempoLivre() {
+        return tempoLivre;
+    }
+
+    public void setTempoLivre(List<TempoLivre> tempoLivre) {
+        this.tempoLivre = tempoLivre;
+    }
+    private String email;
+    private String senha;
+    private String areainteresse;
+    private String departamento;
+    private List<TempoLivre> tempoLivre;
+
+    public void crudTempoLivre() {
+
+    }
 
 }
